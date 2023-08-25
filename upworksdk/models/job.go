1f284ea2058147ea9e8551be74057d3fcc7d2c19
sonnc <19kvh97@gmail.com>
2023-08-25 18:31:36 +0700

    add asMessage function

--- upworksdk/models/job.go
+++ upworksdk/models/job.go
@@ -1,6 +1,13 @@
 package models
 
-import "github.com/PuerkitoBio/goquery"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 type PriceType int
 
@@ -33,7 +40,28 @@ func (job *Job) ImportData(info *goquery.Selection) {
 	job.Title = info.Find(".up-n-link").Text()
 }
 
-func (job *Job) ToString() string {
-	tempalteString := "<b>{{.Title}}</b>\n    {{.Description}}\n    "
-	return ""
+func (job *Job) AsMessage() string {
+	tmplStr := `
+<strong>Title:</strong> {{ .Title | html }}
+<strong>Price Type:</strong> {{ .PriceType | html }}
+<strong>Budget:</strong> {{ .Budget | html }}
+<strong>Description:</strong> {{ .Description | html }}
+<strong>Time Posted:</strong> {{ .TimePosted | html }}
+<strong>Proposal Count:</strong> {{ .ProposalCount | html }}
+`
+	tmpl, err := template.New("jobTemplate").Parse(tmplStr)
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		return ""
+	}
+
+	var result strings.Builder
+	err = tmpl.Execute(&result, job)
+	if err != nil {
+		log.Printf("err : %s", err.Error())
+		return ""
+	}
+
+	return result.String()
+
 }
