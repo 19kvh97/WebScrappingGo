83e09d76c8344c1bdc5e33d6db648c8ae11742c2
kimvnhung <kimvnhung@gmail.com>
2023-10-16 11:29:39 +0700

    add test

--- upworksdk/models/config_test.go
+++ upworksdk/models/config_test.go
@@ -3,9 +3,37 @@ package models
 import (
 	"log"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestDefaultInit(t *testing.T) {
 	config := Config{}
 	log.Printf("config %v", config)
+}
+
+func TestEqualation(t *testing.T) {
+	config1 := Config{
+		Id:   "1",
+		Mode: SYNC_BEST_MATCH,
+		Account: UpworkAccount{
+			Email:    "test@email.com",
+			Password: "test",
+		},
+		State:    NEW_STATE,
+		Interval: 3000,
+	}
+
+	config2 := Config{
+		Id:   "1",
+		Mode: SYNC_BEST_MATCH,
+		Account: UpworkAccount{
+			Email:    "test@email.com",
+			Password: "test",
+		},
+		State:    NEW_STATE,
+		Interval: 3000,
+	}
+
+	require.Equal(t, config1, config2)
 }
